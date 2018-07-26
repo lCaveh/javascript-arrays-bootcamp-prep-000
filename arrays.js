226c7ecefd03c9c28141a0e4992ee42c4afca25d
Kaveh Saleminejad <lcaveh@gmail.com>
2018-07-26 21:33:59 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+function addElementToBeginningOfArray(array, element) {
+  array.unshift("element");
+}
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.push("element");
+}
+
+
+
