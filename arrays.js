c1319cbd8996c0c782ea06a4c90087b38284c2e4
Kaveh Saleminejad <lcaveh@gmail.com>
2018-07-26 22:04:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,6 +5,8 @@ function addElementToBeginningOfArray(array, element) {
 function destructivelyAddElementToBeginningOfArray(array, element) {
    array.unshift(element);
 }
-
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
 
 
