2eae1919c64c091e36607c6fa3ba1508782409e9
Kaveh Saleminejad <lcaveh@gmail.com>
2018-07-26 22:22:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,5 +28,5 @@ function removeElementFromBeginningOfArray(array) {
  return array.slice(1);
 }
 function removeElementFromEndOfArray(array) {
- return array.slice(-1);
+ return array.slice(array.lenght-1);
 }
