06123bd88af323fc1df734d3e2bdd818dfc14654
Kaveh Saleminejad <lcaveh@gmail.com>
2018-07-26 21:50:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element) {
-  array.unshift("element");
+  return [element, ...array]
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.push("element");
