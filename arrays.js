c1da064b46d3dedcd0bf32b018384f14d82f180b
Kaveh Saleminejad <lcaveh@gmail.com>
2018-07-26 22:16:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,3 +20,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
    array.shift();
    return array;
 }
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+   array.shift();
+   return array;
+}
